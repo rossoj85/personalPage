62f4bcf2a457ba8cdb58ecc9340e0f5157e7e996
Jason Rosso <jasonrosso@gmail.com>
2017-11-19 14:37:37 -0500

    removed garbage npm transition installs

--- client/index.js
+++ client/index.js
@@ -2,10 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-
 import Main from './components/main';
-import {useTransitions, withTransition} from 'react-router-transitions';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
 
 
 ReactDOM.render(
