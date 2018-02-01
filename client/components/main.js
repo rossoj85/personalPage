a954cff5254ca951cc8b613e458be9e4f2e8f542
Jason Rosso <jasonrosso@gmail.com>
2018-02-01 00:00:05 -0500

    modularized stylesheets

--- client/components/main.js
+++ client/components/main.js
@@ -1,4 +1,4 @@
-import '../index.scss'
+import '../stylesheets/index.scss'
 import React, { Component } from 'react';
 import { Route, Switch, Fade } from 'react-router-dom';
 import {RainPage, LandingPage, Navbar, Projects, About,Footer, Contact, Loader} from './index';
@@ -17,27 +17,25 @@ export default class Main extends Component{
         console.log(this)
         let navbarOn = true
         return(
-            <div id="mainView">
-           
-            
-            <Switch>
-                <Route  exact path="/" render={(props)=>(
-                    <LandingPage {...props} navbar={Navbar} />
-                )} />
-              <Navbar />
-            </Switch>
-            
-            <Route exact path='/projects' component={Projects} /> 
-            <Route exact path='/projects/rain' component={RainPage} />
-            <Route exact path='/projects/theDesert' component={VRDesert} />
-            <Route exact path='/projects/nightScene' component={VRNightScene} />
-            <Route exact path='/projects/vaporwave' component={VRVaporwave} />
-            <Route exact path='/projects/vangogh' component={VRVangogh} />
-            <Route exact path='/projects/gol' component={Gol} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/contact' component={Contact} />
-        
-            
+            <div id="innerRoute">
+                <div id='innerRouteMainBody'>
+                    <Switch>
+                        <Route  exact path="/" render={(props)=>(
+                            <LandingPage {...props} navbar={Navbar} />
+                        )} />
+                    <Navbar />
+                    </Switch>
+                    
+                    <Route exact path='/projects' component={Projects} /> 
+                    <Route exact path='/projects/rain' component={RainPage} />
+                    <Route exact path='/projects/theDesert' component={VRDesert} />
+                    <Route exact path='/projects/nightScene' component={VRNightScene} />
+                    <Route exact path='/projects/vaporwave' component={VRVaporwave} />
+                    <Route exact path='/projects/vangogh' component={VRVangogh} />
+                    <Route exact path='/projects/gol' component={Gol} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/contact' component={Contact} />
+            </div>
             <Footer />
             </div>
         )
